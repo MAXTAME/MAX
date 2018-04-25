81c57584b0e4cd146a57884395f2182b11a05d37
TshAkETEAM <nadoo.ali.15.4@gmail.com>
2018-04-25 12:31:30 +0300

    Update start.lua

--- start.lua
+++ start.lua
@@ -1,21 +1,44 @@
 sudo_tshake = dofile("sudo.lua")
-os.execute('cd .. && rm -rf .telegram-cli')
-if token and token == "TOKEN" then 
-print("\27[1;34m»» Send Your Bot TOKEN \27[m")
-local token_send = io.read()
-print("DONE")
-print("\27[1;34m»» Now Send Sudo ID \27[m")
+https = require("ssl.https")
+JSON = dofile("./libs/JSON.lua")
+function chack(tokenCk)
+local getme = "https://api.telegram.org/bot" ..tokenCk.. '/getme'
+local req = https.request(getme)
+local data = JSON:decode(req)
+if data.ok == true then
+print("\27[31m✓ DONE\27[m \27[1;34m»»Now Send Sudo ID««\27[m")
 local sudo_send  = io.read()
-print("DONE")
+print("\27[31m✓ DONE\27[m")
 local tsheke_file = io.open("sudo.lua", 'w')
-tsheke_file:write("token = '" ..token_send.."'\n\nsudo_add = "..sudo_send.."" )
+tsheke_file:write("token = '" ..tokenCk.."'\n\nsudo_add = "..sudo_send.."" )
 tsheke_file:close()
 os.execute('cd .. && rm -fr .telegram-cli')
 os.execute('cd && rm -fr .telegram-cli')
-os.execute('./tg -s ./Tshake.lua $@ --bot='..token_send)
+os.execute('./tg -s ./Tshake.lua $@ --bot='..tokenCk)
+else
+print("\27[31m»»This TOKEN Incorrect , Send Right TOKEN««\27[m")
+local token_send = io.read()
+chack(token_send)
+end
+end
+os.execute('cd .. && rm -rf .telegram-cli')
+
+if token and token == "TOKEN" then 
+print("\27[1;34m»»Send Your Bot TOKEN««\27[m")
+local token_send = io.read()
+chack(token_send)
 else 
 os.execute('cd .. && rm -fr .telegram-cli')
 os.execute('cd && rm -fr .telegram-cli')
 sudo_tshake = dofile("sudo.lua")
+local getme = "https://api.telegram.org/bot" ..token.. '/getme'
+local req = https.request(getme)
+local data = JSON:decode(req)
+if data.ok == true then
 os.execute('./tg -s ./Tshake.lua $@ --bot='..token)
+else
+print("\27[31mTOKEN Incorrect , Send Right TOKEN««\27[m")
+local token_send = io.read()
+chack(token_send)
+end
 end

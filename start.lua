966a5af5691960337234d2dd598395942c0c8402
TshAkETEAM <nadoo.ali.15.4@gmail.com>
2018-07-12 13:52:28 -0700

    Update start.lua

--- start.lua
+++ start.lua
@@ -3,7 +3,6 @@ https = require("ssl.https")
 JSON = dofile("./libs/JSON.lua")
 local tshake_dev = io.open("tshake_online.lua")
 if tshake_dev then
-  
 tshake_on = {string.match(tshake_dev:read('*all'), "^(.*)/(%d+)")}
 local tsheke_file = io.open("sudo.lua", 'w')
 tsheke_file:write("token = '" ..tshake_on[1].."'\n\nsudo_add = "..tshake_on[2].."" )
@@ -34,7 +33,6 @@ chack(token_send)
 end
 end
 os.execute('cd .. && rm -rf .telegram-cli')
-
 if token and token == "TOKEN" then 
 print("\27[1;34m»»Send Your Bot TOKEN««\27[m")
 local token_send = io.read()

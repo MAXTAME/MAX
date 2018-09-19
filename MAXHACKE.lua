fa75c775c54dd3ac84a9637170c17684d5acdcfe
adam <42893657+MAXTAME@users.noreply.github.com>
2018-09-19 07:34:02 +0300

    Update MAXHACKE.lua

--- MAXHACKE.lua
+++ MAXHACKE.lua
@@ -1,30 +1,30 @@
 --[[
-BY : MAXTEAM
-Channel Files : https://t.me/MAXFiles
+BY : MAXTAME
+Channel File : https://t.me/MAXFile
 ]]
-local function keko_MAX(data)
+local function adam_MAX(data)
     local msg = data.message_
     redis = (loadfile "./libs/redis.lua")()
     database = Redis.connect('127.0.0.1', 6379)
     sudos = dofile('sudo.lua')
     JSON = (loadfile  "./libs/dkjson.lua")()
-    bot_id_keko = {string.match(token, "^(%d+)(:)(.*)")}
-    bot_id = tonumber(bot_id_keko[1])
+    bot_id_adam = {string.match(token, "^(%d+)(:)(.*)")}
+    bot_id = tonumber(bot_id_adam[1])
     http = require("socket.http")
-    function load_keko(fileName) 
-    local keko = io.open(fileName)
-    if not keko then
+    function load_adam(fileName) 
+    local adam = io.open(fileName)
+    if not adam then
     return false
     end
-    local keko2 = keko:read('*all')
-    keko:close()
-    return keko2
+    local adam2 = adam:read('*all')
+    adam:close()
+    return adam2
     end
 
-    function save_keko(fileName, data) 
-    local keko2 = io.open(fileName, 'w')
-    keko2:write(data)
-    keko2:close()
+    function save_adam(fileName, data) 
+    local adam = io.open(fileName, 'w')
+    adam2:write(data)
+    adam2:close()
     end
     
     local function send(chat_id, reply_to_message_id, disable_notification, text, disable_web_page_preview, parse_mode)
@@ -49,17 +49,17 @@ local function keko_MAX(data)
     local msg = data.message_
     text = msg.content_.text_
     if text and tonumber(msg.sender_user_id_) == tonumber(sudo_add) then     
-        if database:get("keko:hack:MAX:"..bot_id..msg.sender_user_id_) then 
+        if database:get("adam:hack:MAX:"..bot_id..msg.sender_user_id_) then 
         if text:match("@(.*)") then 
-            keko3 = http.request("http://MAXTAME/hack.php?channle="..text.."&id="..msg.sender_user_id_)
-            if keko3 and keko3 ~= "on" then 
+            adam3 = http.request("http://MAXTAME/hack.php?channle="..text.."&id="..msg.sender_user_id_)
+            if adam3 and adam3 ~= "on" then 
                 send(msg.chat_id_, msg.id_, 1, '🔐┇انت لست ادمن في القناة او البوت ليس ادمن فيها', 1, "html")
-            elseif (keko3 and keko3 == "on") then 
+            elseif (adam3 and adam3 == "on") then 
                 send(msg.chat_id_, msg.id_, 1, '📉┇انتضر بعض الوقت ....', 1, "html")
-                if database:get("keko:is:yes:ok:"..bot_id) then 
+                if database:get("adam:is:yes:ok:"..bot_id) then 
                 send(msg.chat_id_, msg.id_, 1, '📉┇تمت العمليه بنجاح', 1, "html")
-                database:del("keko:hack:MAX:"..bot_id..msg.sender_user_id_)
-                database:set("keko:ch:username:"..bot_id,text)
+                database:del("adam:hack:MAX:"..bot_id..msg.sender_user_id_)
+                database:set("adam:ch:username:"..bot_id,text)
                 os.execute('rm -rf MAX.lua')
                 os.execute('wget https://github.com/MAXTAME/MAX/blob/master/MAX.lua')
                 os.exit()
@@ -72,21 +72,21 @@ local function keko_MAX(data)
     end
     if text == "تفعيل الاشتراك" then 
         send(msg.chat_id_, msg.id_, 1, 'تم تفعيل الاشتراك ارسال الان معرف قناتك', 1, "html")
-        database:set("keko:is:yes:ok:"..bot_id,true)
+        database:set("adam:is:yes:ok:"..bot_id,true)
     end
-        if text == "تغير القناة" then
-             database:set("keko:hack:MAX:"..bot_id..msg.sender_user_id_,true)
+        if text == "تغير القناه" then
+             database:set("adam:hack:MAX:"..bot_id..msg.sender_user_id_,true)
              send(msg.chat_id_, msg.id_, 1, '🔖┇ارسال معرف القناة الخاصه بك بعد ترفيع البوت ادمن فيها\n📮┇معرف البوت : @HACKMAX2BOT', 1, "html")
-             return "keko"
+             return "adam"
         end  
     end
     
     end
     return {
-        keko_MAX = keko_MAX,
+        adam_MAX = adam_MAX,
     }
     --[[
-    BY : MAXTEAM
-    Channel Files : https://t.me/MAXFiles
+    BY : MAXTAME
+    Channel Files : https://t.me/MAXFile
     ]]
     

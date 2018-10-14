2b5cb41605289c26839d0735aa7d821fc21f1829
adam <42893657+MAXTAME@users.noreply.github.com>
2018-10-14 22:43:40 +0300

    Add files via upload

---
+++ ar.lua
@@ -0,0 +1,254 @@
+local Tkeko = {}
+-------------------------------------
+Tkeko.BY = "شكر خاص ل @ikeko لكتابه الملف اللغه"
+-------------------------------------
+Tkeko.IsAdd = "المجموعه مفعله سابقا"
+Tkeko.Add = "تم تفعيل المجموعه بنجاح"
+Tkeko.IsUnadd = "المجموعه معطله سابقا"
+Tkeko.unadd = "تم تعطيل المجموعه"
+Tkeko.iscreator = "بالفعل منشئ"
+Tkeko.tocreator = "تم رفعه منشئ"
+Tkeko.isuncreator = "بالفعل تم تنزيله من المنشئين"
+Tkeko.touncreator = "تم تنزيله من المنشئين"
+Tkeko.issudo = "بالفعل مطور" 
+Tkeko.isaddfile = "الملف مفعل مسبقا"
+Tkeko.isunaddfile = "الملف معطل مسبقا"
+Tkeko.okaddreply = "تم اضافه الرد بنجاح"
+Tkeko.notreply = "لا يوجد هاكذا رد"
+Tkeko.delreply = "تم حذف الرد"
+Tkeko.listreply = "قائمه ردود الكروب : "
+Tkeko.repletstoped = "الردود معطله مسبقا"
+Tkeko.repletstop = "تم تعطيل الردود بنجاح"
+Tkeko.repletstarted = "الردود مفعله مسبقا"
+Tkeko.repletstart = "تم تقعيل الردود بنجاح"
+Tkeko.listreplyall = "قائمه ردود العامه : "
+Tkeko.notlistreplyall = "لا يوجد ردود في البوت"
+Tkeko.dellistreplyall = "تم مسح قائمه ردود العامه"
+Tkeko.notlistreply = "لا يوجد ردود في المجموعه"
+Tkeko.dellistreply = "تم مسح قائمه ردود الكروب"
+Tkeko.filenot = "الملف لا يدعم سورس كيكو"
+Tkeko.addfileonf = "تم تفعيل الملف الاضافي بنجاح"
+Tkeko.addfilelang = "تم تفعيل ملف الغه بنجاح"
+Tkeko.unaddfileonf = "تم تعطيل الملف الاضافي بنجاح"
+Tkeko.unaddfilelang = "تم تعطيل ملف الغه بنجاح"
+Tkeko.banpv = "تم حظر المستخدم من التواصل"
+Tkeko.okDellang = "تم حذف ملف من مجلد { langs }"
+Tkeko.notokDellang = "لا يوجد هاكذا ملف في مجلد { langs }"
+Tkeko.okDelfile = "تم حذف ملف من مجلد { files }"
+Tkeko.notokDelfile = "لا يوجد هاكذا ملف في مجلد { files }"
+Tkeko.unbanpv = "تم الغاء حظر المستخدم من التواصل"
+Tkeko.bantext = "تم منع كلمه : "
+Tkeko.isbantext = "الكلمه ممنوعه مسبقا"
+Tkeko.okdelbantext = "تم مسح قائمه الكلمات الممنوعه"
+Tkeko.nobantext = "لا يوجد كلمات ممنوعه"
+Tkeko.listbanetext = "قائمه الكلمات الممنوعه : "
+Tkeko.unbantext = "تم الغاء منع كلمه : "
+Tkeko.isunbantext = "تم الغاء منع كلمه مسبقا"
+Tkeko.isstoppv = "التواصل معطل سابقا"
+Tkeko.stoppv = "تم تعطيل التواصل"
+Tkeko.isunstoppv = "التواصل مفعل سابقا" 
+Tkeko.unstoppv = "تم تعطيل التواصل"
+Tkeko.lagfile = "تم رفع ملف الغه الى مجلد { langs }"
+Tkeko.luafile = "تم رفع ملف الاضافي الى مجلد { files }"
+Tkeko.listfilelng = "قائمه ملفات اللغات : "
+Tkeko.listfilelngad = "<المفعل>"
+Tkeko.notfile = "لا يوجد ملفات"
+Tkeko.listfilmore = "قائمه الملفات الاضافيه : "
+Tkeko.listfilmoread = "<مفعل>"
+Tkeko.witupdate = "جاري رفع الملف"
+Tkeko.tosudo = "تم رفعه مطور"
+Tkeko.isunsudo = "بالفعل تم تنزيله من المطورين"
+Tkeko.tounsudo = "تم تنزيله من المطورين"
+Tkeko.isadmin = "بالفعل ادمن"
+Tkeko.toadmin = "تم رفعه ادمن"
+Tkeko.isunadmin = "بالفعل تم تنزيله من الادمنيه"
+Tkeko.tounadmin = "تم تنزيله من الادمنيه"
+Tkeko.notsudo = "لا يوجد مطورين"
+Tkeko.linkGR = "رابط المجموعه : "
+Tkeko.mut_grlist = "قائمه المقيديين : "
+Tkeko.dellistmut_gr = "تم حذف قائمه المقيديين"
+Tkeko.notmut_grlist = "لا يوجد مقيديين"
+Tkeko.linkGRerre = "ارسال وضع رابط"
+Tkeko.sendlink = "ارسال الرابط الان" 
+Tkeko.pv_stop = "تم تعطيل التواصل مع المطور"
+Tkeko.pv_start = "\nمرحبا بك في بوت حمايه كيكو\n\nارسال ماتيرد وسارسله الى المطور"
+Tkeko.pv_send = "تم ارسال رسالتك الى المطور"
+Tkeko.savelink = "تم حفظ الرابط"
+Tkeko.sudolist = "قائمه المطورين : "
+Tkeko.del_list_sudo = "تم حذف قائمه المطورين"
+Tkeko.isban = "محظور بالفعل"
+Tkeko.ban = "تم حظره من المجموعه"
+Tkeko.unban = "تم الغاء حظره من المجموعه"
+Tkeko.isunban = "غير محظور مسبقا"
+Tkeko.dellistban = "تم حذف قائمه المحظورين"
+Tkeko.deleteadmin = "تم حذف قائمه الادمنيه" 
+Tkeko.isdeleteadmin = "لا يوجد ادمنيه"
+Tkeko.banlist = "قائمه المحظورين : "
+Tkeko.notban = "لا يوجد محظورين"
+Tkeko.dc = "تم عمل اذاعه للمنشور بشكل مباشر"
+Tkeko.dcfwd = "تم عمل اذاعه للمنشور بشكل توجيه"
+Tkeko.clean = "تم تنظيف : "
+Tkeko.notclean = "لا يمكنك تنظيف اكثر من 1000 راساله"
+Tkeko.setphoto = "تم تحديث صوره المجموعه بنجاح"
+Tkeko.setchattitle = "تم تحديث اسم المجموعه بنجاح"
+Tkeko.changeDescription = "تم تحديث وصف المجموعه بنجاح"
+Tkeko.all_add_gr = "عدد الكروبات المفعله : "
+Tkeko.all_gr = "عدد الكروبات الكلي : "
+Tkeko.all_gr_not_add = "عدد الكروبات الغير مفعله : "
+Tkeko.ismute_ge = "مقيد بالفعل"
+Tkeko.mute_ge = "تم تقييده من المجموعه"
+Tkeko.unmute_ge = "تم الغاء تقييده من المجموعه"
+Tkeko.isunmute_ge = "غير مقيد مسبقا"
+Tkeko.dellistmute_ge = "تم حذف قائمه المقيدين"
+Tkeko.mute_gelist = "قائمه المقيدين : "
+Tkeko.notmute_ge = "لا يوجد مقيدين"
+Tkeko.ismute = "مكتوم بالفعل"
+Tkeko.mute = "تم كتمه في البوت"
+Tkeko.isunmute = "غير مكتوم مسبقا"
+Tkeko.unmute = "تم الغاء كتمه في البوت"
+Tkeko.isamdinno = "لا يمكنك عمل ذلك في المدراء"
+Tkeko.setmaxspam = "تم تعيين عدد التكرار : "
+Tkeko.setmaxlist = "تم تعيين عدد احرف الكلايش : "
+Tkeko.user = "المستخدم : "
+Tkeko.erroru = "حدث خطأ"
+Tkeko.notmute = "لا يوجد مكتومين"
+Tkeko.mutelist = "قائمه المكتومين : "
+Tkeko.adminlist = "قائمه الادمنيه : "
+Tkeko.unpinmsg = "تم الغاء التثبيت"
+Tkeko.pinmsg = "تم تثبيت الرساله"
+Tkeko.dellistmute = "تم حذف قائمه المكتومين"
+Tkeko.user_send = "بواسطه : "
+Tkeko.muteongr = "تم تفعيل التقييد"
+Tkeko.ismuteongr = "التقييد مفعل سابقا"
+Tkeko.isunmuteongr = "التقييد معطل سابقا"
+Tkeko.unmuteongr = "تم تعطيل التقييد"
+Tkeko.youlock = "عليه" 
+
+Tkeko.settings = {
+    text = "اعدادات المجموعه : ",
+    text2 = "اعدادات الشخص في المجموعه : ",
+    open = "\n~> { مفتوح }\n",
+    lock = "\n~> { مقفول }\n" ,
+    fwd = "التوجيه",
+    link = "الروابط",
+    chat = "الدردشه",
+    username = "المعرفات",
+    list = "الكلايش",
+    file = "الملفات",
+    sticker = "الملصقات",
+    music = "الصوتيات",
+    markdown = "الماركدون",
+    inlines = "الانلاين",
+    photo = "الصور",
+    bots = "اضافه البوتات",
+    gif = "المتحركه",
+    note = "الاشعارات",
+    join = "الدخول للمجموعه",
+    spam = "التكرار",
+    all = "الكل",
+    pin = "التثبيت",
+} 
+
+Tkeko.lock = { 
+    link = "تم قفل الروابط",
+    islink = "الروابط مقفوله مسبقا",
+    open_link = "تم فتح الروابط", 
+    isopen_link = "الروابط مفتوحه مسبقا",
+    ----------
+    pin = "تم قفل التثبيت",
+    ispin = "التثبيت مقفول مسبقا",
+    open_pin = "تم فتح التثبيت", 
+    isopen_pin = "التثبيت مفتوح مسبقا",
+    ----------
+    join = "تم قفل الدخول للمجموعه",
+    isjoin = "الدخول للمجموعه مقفول مسبقا",
+    open_join = "تم فتح الدخول للمجموعه", 
+    isopen_join = "الدخول للمجموعه مفتوح مسبقا",
+    ---------- 
+    edit_media = "تم قفل تعديل الميديا",
+    isedit_media = "تعديل الميديا مقفول مسبقا",
+    open_edit_media = "تم فتح تعديل الميديا", 
+    isopen_edit_media = "تعديل الميديا مفتوح مسبقا",
+    ----------
+    fwd = "تم قفل التوجيه",
+    isfwd = "التوجيه مقفول مسبقا",
+    open_fwd = "تم فتح التوجيه", 
+    isopen_fwd = "التوجيه مفتوح مسبقا",
+    ----------
+    photo = "تم قفل الصور",
+    isphoto = "الصور مقفوله مسبقا",
+    open_photo = "تم فتح الصور", 
+    isopen_photo = "الصور مفتوحه مسبقا",
+    ----------
+    video = "تم قفل الفديو",
+    isvideo = "الفديوات مقفوله مسبقا",
+    open_video = "تم فتح الفديو", 
+    isopen_video = "الفديوات مفتوحه مسبقا",
+    ----------
+    gif = "تم قفل المتحركه",
+    isgif = "المتحركه مقفوله مسبقا",
+    open_gif = "تم فتح المتحركه", 
+    isopen_gif = "المتحركه مفتوحه مسبقا",
+    ----------
+    username = "تم قفل المعرفات",
+    isusername = "المعرفات مقفوله مسبقا",
+    open_username = "تم فتح المعرفات", 
+    isopen_username = "المعرفات مفتوحه مسبقا",
+    ----------
+    inline = "تم قفل الانلاين",
+    isinline = "الانلاين مقفول مسبقا",
+    open_inline = "تم فتح الانلاين", 
+    isopen_inline = "الانلاين مفتوح مسبقا", 
+    ----------
+    markdown = "تم قفل الماركدون",
+    ismarkdown = "الماركدون مقفول مسبقا",
+    open_markdown = "تم فتح الماركدون", 
+    isopen_markdown = "الماركدون مفتوح مسبقا",  
+    ----------
+    note = "تم قفل الاشعارات",
+    isnote = "الاشعارات مقفول مسبقا",
+    open_note = "تم فتح الاشعارات", 
+    isopen_note = "الاشعارات مفتوح مسبقا",
+    ----------
+    bots = "تم قفل البوتات",
+    isbots = "البوتات مقفوله مسبقا",
+    open_bots = "تم فتح البوتات", 
+    isopen_bots = "البوتات مفتوحه مسبقا",
+    ----------
+    chat = "تم قفل الدردشه",
+    ischat = "الدردشه مقفوله مسبقا",
+    open_chat = "تم فتح الدردشه", 
+    isopen_chat = "الدردشه مفتوحه مسبقا",
+    ----------
+    spam = "تم قفل التكرار",
+    isspam = "التكرار مقفول مسبقا",
+    open_spam = "تم فتح التكرار", 
+    isopen_spam = "التكرار مفتوح مسبقا",
+    ----------
+    sticker = "تم قفل الملصقات",
+    issticker = "الملصقات مقفوله مسبقا",
+    open_sticker = "تم فتح الملصقات", 
+    isopen_sticker = "الملصقات مفتوحه مسبقا",
+    ----------    
+    file = "تم قفل الملفات",
+    isfile = "الملفات مقفوله مسبقا",
+    open_file = "تم فتح الملفات", 
+    isopen_file = "الملفات مفتوحه مسبقا",
+    ----------    
+    music = "تم قفل الصوتيات",
+    ismusic = "الصوتيات مقفوله مسبقا",
+    open_music = "تم فتح الصوتيات", 
+    isopen_music = "الصوتيات مفتوحه مسبقا",
+    ----------    
+    list = "تم قفل الكلايش",
+    islist = "الكلايش مقفوله مسبقا",
+    open_list = "تم فتح الكلايش", 
+    isopen_list = "الكلايش مفتوحه مسبقا",       
+    ----------    
+    all = "تم قفل الكل",
+    isall = "الكل مقفول مسبقا",
+    open_all = "تم فتح الكل", 
+    isopen_all = "الكل مفتوح مسبقا",   
+}
+-------------------------------------
+return Tkeko
